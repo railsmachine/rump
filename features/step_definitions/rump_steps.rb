30f895bb8311ad3a377ba917e9668794b62520a1
Lindsay Holmwood <lindsay@railsmachine.com>
2010-03-09 16:38:10 +1100

    added steps for features

--- features/step_definitions/rump_steps.rb
+++ features/step_definitions/rump_steps.rb
@@ -2,28 +2,76 @@ Given /^I am working in "([^\"]*)"$/ do |directory|
   @basedir = Pathname.new(directory)
 end
 
-Given /^I have an empty git repository named "([^\"]*)"$/ do |reponame|
-  repopath = @basedir.join(reponame)
-  FileUtils.rm_rf(repopath)
-  FileUtils.mkdir_p(repopath)
-  command = "cd #{repopath} && git init -q"
-  system(command)
+Given /^I have an empty git repository named "([^\"]*)"$/ do |repo_name|
+  repo_path = @basedir.join(repo_name)
+  FileUtils.rm_rf(repo_path)
+  FileUtils.mkdir_p(repo_path)
+  
+  Dir.chdir(repo_path) do 
+    commands = ["git init -q", "touch foo", "git add foo", "git commit -qm 'init' ."]
+    commands.each do |command|
+      system(command).should be_true
+    end
+  end
 
-  File.exists?(repopath.join('.git')).should be_true
+  File.exists?(repo_path.join('.git')).should be_true
 end
 
 When /^I run "([^\"]*)"$/ do |cmd|
   if cmd.split(' ').first == "rump"
-    command = "#{ROOT.join('bin', 'rump')} #{cmd.split[2..-1].join(' ')}"
+    command = %w(ruby -rubygems)
+    command << ROOT.join('bin', 'rump')
+    command << cmd.split[1..-1].join(' ')
+    command = command.join(' ')
   else
     command = cmd
   end
 
-  silent_system(command).should be_true
+  Dir.chdir(@basedir) do 
+    system(command).should be_true
+  end
 end
 
-Then /^I should have a git repository at "([^\"]*)"$/ do |reponame|
-  repopath = @basedir.join(reponame)
-  File.exists?(repopath.join('.git')).should be_true
+Then /^I should have a git repository at "([^\"]*)"$/ do |repo_name|
+  repo_path = @basedir.join(repo_name)
+  File.exists?(repo_path.join('.git')).should be_true
 end
 
+Given /^there is no "([^\"]*)" repository$/ do |repo_name|
+  repo_path = @basedir.join(repo_name)
+  FileUtils.rm_rf(repo_path)
+end
+
+Given /^I have a simple Puppet repository named "([^\"]*)"$/ do |repo_name|
+  repo_path = @basedir.join(repo_name)
+  simple_path = ROOT.join('features', 'source', 'simple')
+  hostname = Socket.gethostname
+
+  FileUtils.rm_rf(repo_path)
+  FileUtils.cp_r(simple_path, repo_path)
+
+  File.open(repo_path.join('manifests', 'nodes', "#{hostname}.pp"), 'w') do |f|
+    f << "node #{hostname} { include 'test' }"
+  end
+  
+  Dir.chdir(repo_path) do 
+    commands = ["git init -q", "git add .", "git commit -qm 'init' ."]
+    commands.each do |command|
+      system(command).should be_true
+    end
+  end
+
+  File.exists?(repo_path.join('.git')).should be_true
+end
+
+Then /^I should see a file at "([^\"]*)"$/ do |path|
+  File.exists?(path).should be_true
+end
+
+Given /^there is no "([^\"]*)" file$/ do |file|
+  FileUtils.rm_rf(file).should be_true
+end
+
+Given /^"([^\"]*)" is installed on my system$/ do |command|
+  system("which #{command}").should be_true
+end
