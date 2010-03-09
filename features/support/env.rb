04d8a4609ee4a3c3364fcf97b556e3bc60ff7b8e
Lindsay Holmwood <lindsay@railsmachine.com>
2010-03-09 16:37:57 +1100

    don't join the command, as system can handle an array. require 'socket' so we can get the hostname later on

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,8 +1,10 @@
 #!/usr/bin/env ruby 
 
+require 'socket'
+
 ROOT = Pathname.new(File.expand_path(File.join(File.dirname(__FILE__), '..', '..')))
 
 def silent_system(cmd)
-  command = [cmd, "2>&1 /dev/null"].join(' ')
+  command = [cmd, "2>&1 /dev/null"]
   system(command)
 end
