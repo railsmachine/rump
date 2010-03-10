c45a3927c85a074598c446a3d99d316d9b154a33
Lindsay Holmwood <lindsay@railsmachine.com>
2010-03-10 12:11:06 +1100

    added action for freezing dependencies using git. do smarter command line generation now frozen deps are possible

--- lib/rump.rb
+++ lib/rump.rb
@@ -16,6 +16,11 @@ end
 
 class Rump < Thor
 
+  def initialize
+    super
+    @root = Pathname.new(Dir.getwd)
+  end
+    
   desc "clone repository [directory]", "clone a Git repository of Puppet manifests"
   def clone(repository, directory=nil)
     command = "git clone -q #{repository} #{directory}"
@@ -28,24 +33,44 @@ class Rump < Thor
       puts "You should probably be root when running this! Proceeding anyway..." 
     end
     
-    unless system("which puppet > /dev/null")
-      puts "You don't have Puppet installed!"
-      exit 2
+    args = []
+    if has_frozen_components?
+      args << "ruby" 
+      args << "-I #{@root.join('vendor', 'facter', 'lib')}" 
+      args << "-I #{@root.join('vendor', 'puppet', 'lib')}"
+      args << "#{@root.join('vendor', 'puppet', 'bin', 'puppet')}"
+    else
+      unless system("which puppet > /dev/null")
+        puts "You don't have Puppet installed!"
+        puts "Please either install it on your system or freeze it with 'rump freeze'"
+        exit 2
+      end
+      args << "puppet"
     end
-    
-    root = Pathname.new(Dir.getwd)
-    
-    args = %w(puppet)
-    args << "--modulepath #{root.join('modules')}"
-    args << "--confdir #{root.join('etc')}" unless puppet_args.include?("--confdir")
-    args << "--vardir #{root.join('var')}" unless puppet_args.include?("--vardir")
-    args << "#{root.join('manifests', 'site.pp')}"
+    args << "--modulepath #{@root.join('modules')}"
+    args << "--confdir #{@root.join('etc')}" unless puppet_args.include?("--confdir")
+    args << "--vardir #{@root.join('var')}" unless puppet_args.include?("--vardir")
+    args << "#{@root.join('manifests', 'site.pp')}"
+
     args += puppet_args
 
     command = args.join(' ')
     puts command if args.include?("--debug")
-    system(command)
+    system(command) ? exit(0) : exit(1)
   end
+
+  desc "freeze repository project", "freeze Puppet into your manifests repository"
+  def freeze(project, repository)
+    command = "git submodule add #{repository} #{@root.join('vendor', project)}"
+    system(command) ? exit(0) : exit(1)
+  end
+
+  private 
+  def has_frozen_components?
+    vendored = Dir.glob("#{@root.join('vendor')}/*").map {|v| v.split('/').last}
+    vendored.include?("puppet") && vendored.include?("facter")
+  end
+
 
 end
 
